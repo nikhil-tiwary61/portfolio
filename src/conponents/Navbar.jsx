dce0002ef963ee14f1d7d134bc6918b01abbfca5
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-03 16:19:40 +0530

    add links to the Navbar component

--- src/conponents/Navbar.jsx
+++ src/conponents/Navbar.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-scroll";
+
 export default function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <div className="name">Nikhil Tiwary</div>
-        <div className="desktop-menu"></div>
-        <button></button>
+        <div className="name">
+          <Link>Nikhil Tiwary</Link>
+        </div>
+        <div className="desktop-menu">
+          <Link className="desktop-menu-list-items">Home</Link>
+          <Link className="desktop-menu-list-items">About</Link>
+          <Link className="desktop-menu-list-items">Skills</Link>
+          <Link className="desktop-menu-list-items">Projects</Link>
+        </div>
+        <button>Contact Me</button>
       </nav>
     </>
   );
