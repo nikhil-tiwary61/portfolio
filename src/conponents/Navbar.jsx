dbfa9abb6e112246e5c1254068212a5ac9471d06
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-03 16:11:43 +0530

    add navbar component

---
+++ src/conponents/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="name">Nikhil Tiwary</div>
+        <div className="desktop-menu"></div>
+        <button></button>
+      </nav>
+    </>
+  );
+}
