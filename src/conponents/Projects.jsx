11f390ba80623638ae8a21c0d226a7fc096a87f3
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-16 13:16:39 +0530

    add style to Projects component along with responsivity

--- src/conponents/Projects.jsx
+++ src/conponents/Projects.jsx
@@ -3,43 +3,47 @@ import { projects } from "../../data/data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="blue-background">
-      <h2>Projects</h2>
-      {projects.map((project, index) => {
-        return (
-          <div className="card" key={index}>
-            <div className="card-img">
-              <img src={project.img} alt="Project Image" />
-            </div>
-            <div className="card-text">
-              <h2>{project.title}</h2>
-              <p>{project.desc}</p>
-              <br />
-              <p>
-                <span id="tech-used">Made with:</span>{" "}
-                {project.tech.map((technology, index) => (
-                  <img
-                    key={index}
-                    src={technology}
-                    alt={technology}
-                    className="tech-icons"
-                  />
-                ))}
-              </p>
-              <br />
-              <p>{project.date}</p>
-            </div>
-            <div className="card-btn">
-              <a href={project.demo} target="_blank" rel="noreferrer">
-                <button>Demo</button>
-              </a>
-              <a href={project.code} target="_blank" rel="noreferrer">
-                <button>Code</button>
-              </a>
-            </div>
-          </div>
-        );
-      })}
+    <section className="blue-background">
+      <div id="projects">
+        <h2>Projects</h2>
+        <div id="project-grid">
+          {projects.map((project, index) => {
+            return (
+              <div className="card" key={index}>
+                <div className="card-img">
+                  <img src={project.img} alt="Project Image" />
+                </div>
+                <div className="card-text">
+                  <h3>{project.title}</h3>
+                  <p>{project.desc}</p>
+                  <br />
+                  <p>
+                    <span id="tech-used">Made with:</span>{" "}
+                    {project.tech.map((technology, index) => (
+                      <img
+                        key={index}
+                        src={technology}
+                        alt={technology}
+                        className="tech-icons"
+                      />
+                    ))}
+                  </p>
+                  <br />
+                  <p>{project.date}</p>
+                </div>
+                <div className="card-btn">
+                  <a href={project.demo} target="_blank" rel="noreferrer">
+                    <button>Demo</button>
+                  </a>
+                  <a href={project.code} target="_blank" rel="noreferrer">
+                    <button>Code</button>
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 }
