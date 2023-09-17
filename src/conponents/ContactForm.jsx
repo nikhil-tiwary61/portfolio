a83b2f3bf9fe4d1dce5e848b99fe8609883d02d0
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-18 01:33:44 +0530

    feat: contact via emails by filling contact form using emailjs

--- src/conponents/ContactForm.jsx
+++ src/conponents/ContactForm.jsx
@@ -1,17 +1,51 @@
 import "../styles/ContactForm.css";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_5ht1f2z",
+        "template_a5xyqyc",
+        form.current,
+        "Ys1ADlNIfHwE6GyWY"
+      )
+      .then(
+        (result) => {
+          e.target.reset();
+          alert("Email Sent");
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section id="contact-me">
       <h2>
         Contact Form <span className="line"></span>
       </h2>
-      <form className="contact-form">
+      <form className="contact-form" ref={form} onSubmit={sendEmail}>
         <legend className="contactDesc">
           Please fill out the form below to discuss any work opportunities.
         </legend>
-        <input type="text" className="client-name" placeholder="Your Name" />
-        <input type="email" className="email" placeholder="Your Email" />
+        <input
+          type="text"
+          className="client-name"
+          placeholder="Your Name"
+          name="from_name"
+        />
+        <input
+          type="email"
+          className="email"
+          placeholder="Your Email"
+          name="from_email"
+        />
         <textarea
           name="message"
           rows="5"
