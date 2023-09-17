ee01d2c68f0657670c14823994b88177b5d793fd
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-17 13:07:49 +0530

    add projects to Projects component

--- data/data.jsx
+++ data/data.jsx
@@ -4,13 +4,16 @@ import CSS3 from "../src/assets/skills/CSS3.png";
 import JAVASCRIPT from "../src/assets/skills/JAVASCRIPT.png";
 import REACT from "../src/assets/skills/REACT.png";
 import REDUX from "../src/assets/skills/REDUX.png";
+import REACTROUTER from "../src/assets/skills/REACT-ROUTER.png";
 import NODE from "../src/assets/skills/NODE.png";
 import EXPRESS from "../src/assets/skills/EXPRESS.png";
 import MONGODB from "../src/assets/skills/MONGODB.png";
 import MONGOOSE from "../src/assets/skills/MONGOOSE.png";
+import FIREBASE from "../src/assets/skills/FIREBASE.png";
 import GIT from "../src/assets/skills/GIT.png";
 import GITHUB from "../src/assets/skills/GITHUB.png";
-import REACTROUTER from "../src/assets/skills/REACT-ROUTER.png";
+
+import ECOMMERCE from "../src/assets/projectPreviews/ECOMMERCE.jpeg";
 
 export const technologies = [
   {
@@ -30,6 +33,7 @@ export const technologies = [
       { name: "EXPRESS", icon: EXPRESS },
       { name: "MONGODB", icon: MONGODB },
       { name: "MONGOOSE", icon: MONGOOSE },
+      { name: "FIREBASE", icon: FIREBASE },
     ],
   },
   {
@@ -43,12 +47,57 @@ export const technologies = [
 
 export const projects = [
   {
-    img: dummyImage,
     title: "E-commerce App",
     desc: "Features: Add to cart, Search bar, Filter by category",
     tech: [REACT, REACTROUTER],
-    date: "Aug 2023",
+    img: ECOMMERCE,
+    date: "August 2023",
     demo: "https://shopping-cart-neon-psi.vercel.app/",
     code: "https://github.com/nikhil-tiwary61/shopping-cart",
+  },
+  {
+    title: "CV Builder",
+    desc: "Features: Build resume, Print the resume",
+    tech: [REACT],
+    img: dummyImage,
+    date: "August 2023",
+    demo: "https://cv-builder-theta-ecru.vercel.app/",
+    code: "https://github.com/nikhil-tiwary61/cv-builder",
+  },
+  {
+    title: "Gossip - Real time chat app",
+    desc: "Features: Google Sign In",
+    tech: [REACT, FIREBASE],
+    img: dummyImage,
+    date: "August 2023",
+    demo: "https://gossip-kappa.vercel.app/",
+    code: "https://github.com/nikhil-tiwary61/gossip",
+  },
+  {
+    title: "Tic-Tac-Toe",
+    desc: "Collaborated with the back-end team to make the front-end for the project",
+    tech: [JAVASCRIPT, HTML5, CSS3],
+    img: dummyImage,
+    date: "May 2023",
+    demo: "https://nikhil-tiwary61.github.io/odin-tic-tac-toe/",
+    code: "https://github.com/nikhil-tiwary61/odin-tic-tac-toe",
+  },
+  {
+    title: "Calculator",
+    desc: "A calculator app for basic mathematical operations",
+    tech: [JAVASCRIPT, HTML5, CSS3],
+    img: dummyImage,
+    date: "June 2023",
+    demo: "https://nikhil-tiwary61.github.io/calculator/",
+    code: "https://github.com/nikhil-tiwary61/calculator",
+  },
+  {
+    title: "Rock-Paper-Scissor",
+    desc: "Popular rock-paper-scissor game, best of 5",
+    tech: [JAVASCRIPT, HTML5, CSS3],
+    img: dummyImage,
+    date: "March 2023",
+    demo: "https://nikhil-tiwary61.github.io/rock-paper-scissors/",
+    code: "https://github.com/nikhil-tiwary61/rock-paper-scissors",
   },
 ];
