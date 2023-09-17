bc411adbeaeb1cc7fca91b9ad2ca7f9d08dd1d58
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-17 19:26:46 +0530

    feat: add burger menu in navbar

--- src/conponents/Navbar.jsx
+++ src/conponents/Navbar.jsx
@@ -1,8 +1,11 @@
 import "../styles/Navbar.css";
 import { Link } from "react-scroll";
 import contactMe from "../assets/contact_me.png";
+import menu from "../assets/menu.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav>
       <div className="navbar">
@@ -26,7 +29,7 @@ export default function Navbar() {
             to="about"
             spy={true}
             smooth={true}
-            offset={0}
+            offset={-62}
             duration={500}
             className="desktop-menu-list-items"
           >
@@ -37,7 +40,7 @@ export default function Navbar() {
             to="skills"
             spy={true}
             smooth={true}
-            offset={0}
+            offset={-62}
             duration={500}
             className="desktop-menu-list-items"
           >
@@ -48,7 +51,7 @@ export default function Navbar() {
             to="projects"
             spy={true}
             smooth={true}
-            offset={0}
+            offset={-62}
             duration={500}
             className="desktop-menu-list-items"
           >
@@ -59,6 +62,66 @@ export default function Navbar() {
           <img src={contactMe} alt="Contact Me Icon" />
           Contact Me
         </button>
+
+        <img
+          src={menu}
+          alt="Menu"
+          className="mob-menu"
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        <div
+          className="nav-menu"
+          style={{ display: showMenu ? "flex" : "none" }}
+        >
+          <Link
+            activeClass="active"
+            to="intro"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+            className="list-items"
+            onClick={() => setShowMenu(false)}
+          >
+            Home
+          </Link>
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-62}
+            duration={500}
+            className="list-items"
+            onClick={() => setShowMenu(false)}
+          >
+            About
+          </Link>
+          <Link
+            activeClass="active"
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={-62}
+            duration={500}
+            className="list-items"
+            onClick={() => setShowMenu(false)}
+          >
+            Skills
+          </Link>
+          <Link
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-62}
+            duration={500}
+            className="list-items"
+            onClick={() => setShowMenu(false)}
+          >
+            Projects
+          </Link>
+        </div>
       </div>
     </nav>
   );
