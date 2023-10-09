ec48fe682bb9359516bb13437c93b7ecac54cc3e
Nikhil Tiwary <ntiwary61@gmail.com>
2023-10-09 23:50:59 +0530

    add ThemeContext to the app

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,26 @@ import Skills from "./conponents/Skills";
 import Projects from "./conponents/Projects";
 import ContactForm from "./conponents/ContactForm";
 import Footer from "./conponents/Footer";
+import ThemeContext from "./context/ThemeContext";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("darkMode");
+  function handleClick() {
+    setTheme(theme === "darkMode" ? "lightMode" : "darkMode");
+    console.log(theme);
+  }
   return (
-    <>
+    <ThemeContext.Provider value={theme}>
       <Navbar />
+      <button onClick={handleClick}></button>
       <Home />
       <About />
       <Skills />
       <Projects />
       <ContactForm />
       <Footer />
-    </>
+    </ThemeContext.Provider>
   );
 }
 
